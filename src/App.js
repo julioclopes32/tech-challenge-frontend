caf805116a8773a82ca09e8708ba6a49cbd62565
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2021-11-22 12:23:00 -0300

    reestart

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import api from './services/api';
 
 function App() {
   return (
     <div className="App">
-      <head>
-          <title>MovieWiki</title>
-          <meta charset="UTF-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;500;700&display=swap" rel="stylesheet"/>  
-          <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@400;500;600&display=swap" rel="stylesheet"/>
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"/>
-          <link rel="stylesheet" href="/stylesheets/homePage.css"/>
-      </head>
-      <div className="back"></div>
-      <div className="filter"></div>
-      <div className="brand"> 
-        <a href="#"><i className="fas fa-video"></i> MovieWiki</a>
-      </div>
-      <div className="container">
-        <p class="search">Search Movie </p>
-        <div className="form">
-            <input type="text" placeholder="Enter Movie name or keyword" name="movie" required=""/>
-            <input type="Submit" value="Find"/>
-        </div>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
