56d611e1b8863b6a91acb85c18ffa30e3f9248ac
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2021-11-27 13:42:30 -0300

    Update Signin.js

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,35 +1,64 @@
-import React from 'react'
-import { useState } from 'react';
+import React, {Component} from 'react'
 import { auth } from '../firebase';
-import {useHistory} from 'react-router-dom';
 import './login.css';
+import ReactDOM from 'react-dom'; 
+import { withRouter } from "react-router-dom";
 
-const Signin = () => {
-    const history = useHistory();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPass, setConfirmPass] = useState("");
 
-    function validateEmail(email) {
+class Signin extends Component {
+    constructor(props){
+        super(props);
+        props = this.props
+    }
+
+    state = {
+        alertText: "ALERTA!",
+        username : "",
+        password : "",
+        confirmPass: "",
+    };
+
+    getAlertText(){
+        return(this.state.alertText)
+    }
+
+    hideAlert(){
+        this.alerta.style.display='none';
+    }
+
+    showAlert(text){
+        this.setState({alertText:text});
+        this.alerta.style.display='block';
+    }
+
+    getUsername(){
+        return(this.state.username)
+    }
+
+    getPassword(){
+        return(this.state.password)
+    }
+
+    getConfirmPass(){
+        return(this.state.confirmPass)
+    }
+
+    validateEmail(email) {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
 
-    function HomeButton() {
-        history.push('/')
-    };
-
-    function LoginButton() {
-        history.push('/login')
-    };
-
-    const signUp = e => {
+    signUp(e){
+        this.hideAlert();
+        const username = this.getUsername();
+        const password = this.getPassword();
+        const confirmPass = this.getConfirmPass();
         if(username === "" || password === "" || confirmPass === ""){
-            alert("Fill all fields!")
+            this.showAlert("Fill all fields!")
         }else if(confirmPass !== password){
-            alert("passwords don't match!")
-        }else if(!validateEmail(username)){
-            alert("Email baddly formatted!")
+            this.showAlert("passwords don't match!")
+        }else if(!this.validateEmail(username)){
+            this.showAlert("Email baddly formatted!")
         }
         else{
             e.preventDefault();
@@ -37,60 +66,95 @@ const Signin = () => {
                 username,
                 password
             ).then(user=>{
-                alert("Successfully Registered!")
+                alert("Successfull Registered")
+                this.signIn()
             }).catch(err=>{
-                alert(err)
+                //this.showAlert(err)
+                this.showAlert(err.toString())
             })
         }
     }
 
-    return (
+    signIn(){
+        this.hideAlert();
+        const username = this.getUsername();
+        const password = this.getPassword();
+        if(username === "" || password === ""){
+            this.showAlert("Fill all fields!")
+        }else if(!this.validateEmail(username)){
+            this.showAlert("Email baddly formatted!");
+        }
+        else{
+            auth.signInWithEmailAndPassword(
+                username,
+                password
+            ).then(user=>{
+                this.showAlert("Successfully Logged!");
+                this.props.history.push('/');
+            }).catch(err=>{
+                this.showAlert(err.toString())
+            })
+        }
+    }
+
+    async componentDidMount() {
+        var alerta = ReactDOM.findDOMNode(this.alerta);
+        console.log(alerta)
+    }
+
+    render() {
+        return (
         <div className="home">
-      <div className="brand" onClick={HomeButton}> 
-        <p><i className="fas fa-video"></i> Fleye-Tech-Challenge</p>
-      </div>
-      <div className="container">
-        <p className="search">Sign-in </p>
-        <div className="userform">
-            <input
-            type="text"
-            placeholder="Email"
-            defaultValue = "Email"
-            name="movie"
-            required=""
-            onChange={(e) => {
-                setUsername(e.target.value);
-            }}/>
-            <br/>
-            <br/>
-            <input
-            type="text"
-            placeholder="Password"
-            defaultValue = "Password"
-            required=""
-            onChange={(e) => {
-                setPassword(e.target.value);
-            }}/>
-            <br/>
-            <br/>
-            <input
-            type="text"
-            placeholder="Confirm Password"
-            defaultValue = "Confirm Password"
-            required=""
-            onChange={(e) => {
-                setConfirmPass(e.target.value);
-            }}/>
-            <br/>
-            <br/>
-            <input id="login-btn"
-            type="Submit" 
-            defaultValue = "Sign-Up"
-            onClick={signUp}/>
+        <div className="brand" onClick={()=>this.props.history.push('/')}> 
+            <p><i className="fas fa-video"></i> Fleye-Tech-Challenge</p>
         </div>
-        <p id="login-register" onClick={LoginButton}>go to Login!</p>
-      </div>
-    </div>
-    )
+        <div className="container">
+            <p className="search">Sign-in </p>
+            <div className="userform">
+                <input
+                type="text"
+                placeholder="Email"
+                defaultValue = "Email"
+                name="movie"
+                required=""
+                onChange={(e) => {
+                    this.setState({username:e.target.value});
+                }}/>
+                <br/>
+                <br/>
+                <input
+                type="text"
+                placeholder="Password"
+                defaultValue = "Password"
+                required=""
+                onChange={(e) => {
+                    this.setState({password:e.target.value});
+                }}/>
+                <br/>
+                <br/>
+                <input
+                type="text"
+                placeholder="Confirm Password"
+                defaultValue = "Confirm Password"
+                required=""
+                onChange={(e) => {
+                    this.setState({confirmPass:e.target.value});
+                }}/>
+                <br/>
+                <br/>
+                <input id="login-btn"
+                type="Submit" 
+                defaultValue = "Sign-Up"
+                onClick={(e)=>{this.hideAlert();setTimeout(() => {this.signUp((e))}, 1000);}}/>
+                <div id="alerta" className="alert" ref={alerta => this.alerta = alerta}>
+                    {this.getAlertText()}
+                </div>
+            </div>
+            <p id="login-register" onClick={()=>this.props.history.push('/login')}>go to Login!</p>
+            </div>
+        </div>
+        )
+    }
 }
-export default Signin
+
+export default withRouter(Signin);
